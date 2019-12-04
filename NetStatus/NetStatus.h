83639b7009daedae105e3afdedcb1120e09a81b0
khusboo.suhasini <khusboo.suhasini@techjini.com>
2019-12-04 12:46:10 +0530

    Added framework source and podspec

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by datamatics on 03/12/19.
+//  Copyright © 2019 datamatics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
